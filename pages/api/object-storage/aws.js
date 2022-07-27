a896d1fa45b00065bdb4d3dd50aacaf9c6dbb126
Pranav Shikarpur <contact@snpranav.com>
2022-07-27 02:18:28 -0400

    fix: fixed bugs with vercel environment conflicts.

--- pages/api/object-storage/aws.js
+++ pages/api/object-storage/aws.js
@@ -2,8 +2,8 @@ import S3 from "aws-sdk/clients/s3";
 
 const s3 = new S3({
   region: process.env.AWS_S3_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS,
   signatureVersion: "v4",
 });
 
